2520a8dd101319bec6e654689ff5ebb5d21402ef
Henrique Weiand <henriqueweiand@gmail.com>
2020-09-14 16:20:13 -0300

    add styled-components, tests component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const Box = styled.View`
+  background: #999;
+  padding: 50px;
+`;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Box>
+        <Text>Hello</Text>
+      </Box>
     </View>
   );
 }
@@ -14,8 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
